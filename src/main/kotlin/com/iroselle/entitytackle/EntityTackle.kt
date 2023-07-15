3680dae6058b00b486f506e77de24e38f5ba5744
Score2 <score2.dev@gmail.com>
2023-07-15 12:04:12 +0800

    init

---
+++ src/main/kotlin/com/iroselle/entitytackle/EntityTackle.kt
@@ -0,0 +1,11 @@
+package com.iroselle.entitytackle
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object EntityTackle : Plugin() {
+
+    override fun onEnable() {
+        info("Successfully running ExamplePlugin!")
+    }
+}
